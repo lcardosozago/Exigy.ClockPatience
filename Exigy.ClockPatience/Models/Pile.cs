5f908fd6462c3e02004f2ebf0ffbaa667ac72a1e
Lucas Cardoso Zago <lucas.zago@ms.senac.br>
2023-09-21 06:59:33 -0400

    Creating Pile model

---
+++ Exigy.ClockPatience/Models/Pile.cs
@@ -0,0 +1,14 @@
+﻿namespace Exigy.ClockPatience.Models
+{
+    public class Pile
+    {
+        public int PositionId { get; private set; }
+        public IList<Card> Cards { get; private set; }
+
+        public Pile(int positionId)
+        {
+            PositionId = positionId;
+            Cards = new List<Card>();
+        }
+    }
+}
