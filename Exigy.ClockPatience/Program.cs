88752506b5c1b59eb45a83546502b83b4cecbe35
Lucas Cardoso Zago <lucas.zago@ms.senac.br>
2023-09-21 07:15:25 -0400

    Validating if input has the amount of cards expected

--- Exigy.ClockPatience/Program.cs
+++ Exigy.ClockPatience/Program.cs
@@ -1,6 +1,9 @@
 ﻿string inputStringLine;
 var concatStringResult = "";
 
+var maxNumberOfPiles = 13;
+var maxNumberOfCardsInAPile = 4;
+
 try
 {
     do
@@ -15,7 +18,11 @@ try
     var reversedCardsInputList = concatStringResult.Split(" ").Reverse().ToList();
     reversedCardsInputList.Remove("");
 
+    var totalCardsInDeck = maxNumberOfPiles * maxNumberOfCardsInAPile;
+
     Console.WriteLine(reversedCardsInputList.Count);
+
+    if (reversedCardsInputList.Count != totalCardsInDeck) throw new NotImplementedException("Invalid number of cards provided.");
 }
 catch (Exception e)
 {
